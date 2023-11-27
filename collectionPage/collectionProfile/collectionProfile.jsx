f2ab05f64109d11544f95ebf527641807d563159
Madwarbit <149225346+Madwarbit@users.noreply.github.com>
2023-11-28 01:21:30 +0500

    Update collectionProfile.jsx

--- collectionPage/collectionProfile/collectionProfile.jsx
+++ collectionPage/collectionProfile/collectionProfile.jsx
@@ -42,12 +42,9 @@ const collectionProfile = () => {
         </div>
 
         <div className={Style.collectionProfile_box_middle}>
-          <h1>Awesome NFTs Collection</h1>
+          <h1>Mad-Rabbbitz NFTs Collection</h1>
           <p>
-            Karafuru is home to 5,555 generative arts where colors reign
-            supreme. Leave the drab reality and enter the world of Karafuru by
-            Museum of Toys.
-          </p>
+Welcome to the whimsical world of Mad-Rabbitz. We invite you to embark on a captivating journey into a fantastical universe inhabited by humanoid rabbits. Blending artistry, fantasy, and blockchain technology, our collection aims to bring joy, intrigue, and endless possibilities to the NFT space.          </p>
 
           <div className={Style.collectionProfile_box_middle_box}>
             {cardArray.map((el, i) => (
