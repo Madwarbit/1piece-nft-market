f04a6ca9c1793bed474f340fe7c4b46e207f06ad
Madwarbit <149225346+Madwarbit@users.noreply.github.com>
2023-11-29 08:43:41 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://polygon-mumbai.g.alchemy.com/v2/7zNpSwfaOC6XqkamL9_UX_zxA_cURtbC";
-const NEXT_PUBLIC_PRIVATE_KEY = "b1ea4b57668c935d28dbdd8a1147707dda3467bfce0b34053bb90791aa87fa27";
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "";
+const NEXT_PUBLIC_PRIVATE_KEY = "";
 
 module.exports = {
   solidity: "0.8.17",
